886c6e7f5e7efd3f4b51e1bd022ae6f40d6c303c
Dmytro Kuzovlev <kuzovlev.dm@gmail.com>
2022-05-21 18:59:05 +0300

    Section visual-entry v1

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,29 @@ document.addEventListener("DOMContentLoaded", function () {
   const menu = document.querySelector(".menu__wrapper");
   const menuButtonToggler = document.querySelector(".menu__mobile");
 
-    menuButtonToggler.addEventListener("click", () => {
-      menu.classList.toggle("menu__mobile_shown");
+  menuButtonToggler.addEventListener("click", () => {
+    menu.classList.toggle("menu__mobile_shown");
+    dropDownMenu.forEach((item) => {
+        item.classList.remove('expanded');
+    })
+  });
+
+  function calcEntryBlockHeight() {
+    const entryImageHeight =
+      document.querySelector(".visual-entry__img").clientHeight;
+    const visualEntryUsp = document.querySelectorAll(".visual-entry__usp-item");
+    visualEntryUsp.forEach((item) => {
+      item.style.setProperty("--elemHeight", entryImageHeight / 4 + "px");
     });
+  }
+
+  calcEntryBlockHeight();
+
+  window.addEventListener(
+    "resize",
+    () => {
+      if (window.innerWidth > 768) calcEntryBlockHeight();
+    },
+    true
+  );
 });
